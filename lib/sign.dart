628e74be604fa6cbfd298b03d9892ee62886314a
abdullah0707 <abdullah_elgammal@yahoo.com>
2023-07-13 15:39:30 +0300

    -M "edit name file singUp"

--- lib/sign.dart
+++ lib/sign.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'main.dart';
+import 'main_test_sing_up.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Sing extends StatefulWidget {
